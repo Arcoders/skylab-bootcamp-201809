d147c51a545e8b40f3a7c6ed8899dd94613022cb
Ismael Haytam <anonismaa@gmail.com>
2018-10-04 16:35:22 +0200

    done!

--- staff/ismael-haytam/safe-box/safe-box.js
+++ staff/ismael-haytam/safe-box/safe-box.js
@@ -1,18 +1,14 @@
 var safeBox = {
 
     saveSecret: function(secret, password) {
-        console.log('your secret was encrypted: ' + window.btoa(secret+password)); 
-        return this.retrieveSecret(secret);
+        if (!password || !password.length || !password.trim()) throw Error('invalid password');
+        this.secret = secret;
 	},
 
     retrieveSecret: function(password) {
-        console.log(secret)
+        return this.secret
     }
 }
 
-
-safeBox.saveSecret('My new secret', 'ismael-1995');
-
-safeBox.retrieveSecret('ismael-1995');
 
 
